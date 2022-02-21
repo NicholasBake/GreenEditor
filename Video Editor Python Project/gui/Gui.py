19f19fc1c07011a2c61b212a768fdaa032a48cc2
NicholasBake <99704076+NicholasBake@users.noreply.github.com>
2022-02-21 17:49:30 -0500

    Update Gui.py

--- Video Editor Python Project/gui/Gui.py
+++ Video Editor Python Project/gui/Gui.py
@@ -1,32 +1,57 @@
-
 from asyncio import sleep
+from multiprocessing import Process
+from threading import Thread
+import time
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QFont
 import sys
 
-class WINDOW():
+class WINDOW(QWidget):
+    
     sizeX = 1280
     sizeY = 720
     positionX = 0
     positionY = 0
     name = "Green Editor"
+    app = QApplication(sys.argv)
+    window = QWidget()
     def __init__(self, sizeX = 1280,sizeY = 720, positionX = 0, positionY = 0, name = "Green Editor"):
         self.sizeX = sizeX
         self.sizeY = sizeY
         self.positionX = positionX
         self.positionY = positionY
         self.name = name
-        app = QApplication(sys.argv)
-        window = QWidget()
-        window.setWindowTitle(self.name)
-        window.resize(self.sizeX, self.sizeY)
-        window.move(self.positionX, self.positionY)
-        label = QLabel(window)
-        label.setText("Hello world!")
-        window.show()
-        app.exec()
+        super().__init__()
+    def loadWindow(self):
+        self.window.setWindowTitle(self.name)
+        self.window.resize(self.sizeX, self.sizeY)
+        self.window.move(self.positionX, self.positionY)
+        self.window.show()
+        self.app.exec()
+
+
+def setupWidget(widgetToAdd, x = 200, y = 200, w = 200, h = 200, labelText = "", labelFont = 0):
+    widgetToAdd.move(x, y)
+    widgetToAdd.resize(w, h)
+    if labelText != "":
+        widgetToAdd.setText(labelText)
+        if labelFont != 0:
+            widgetToAdd.setFont(QFont('Times', labelFont))
+        else:
+            widgetToAdd.setFont(QFont('Times', 10))
+    widgetToAdd.show()
+
 
 def guiLoad():
-    x = WINDOW(sizeX= 200)
-    sleep(1)
+    editorWindow = WINDOW(positionX = 200, sizeX = 1280)
+    test = QLabel(editorWindow.window)
+    test.setFont(QFont())
+    setupWidget(test, labelText = "Hello world!", labelFont= 20)
+    editorWindow.loadWindow()
+    time.sleep(1)
     
     
+    
+    
+    
+
